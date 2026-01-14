de7033405dd45c19eb63fc3be31fa585913fbbf1
Carl Otto Strömstedt <carl.stroemstedt@open-circle.ch>
2026-01-14 11:36:47 +0100

    feat: add normalized response parsing to API requests

--- lib/unifi/client/guests.rb
+++ lib/unifi/client/guests.rb
@@ -3,11 +3,11 @@ module Unifi
   class Client
 
     module Guests
+      include ResponseNormalizer
 
       def list_guests(within = 8760, site = @site)
         body = { within: within }
-        response = self.class.get("/s/#{site}/stat/guest", { body: body.to_json })
-        response.parsed_response
+        normalize_unifi_response(self.class.get("/s/#{site}/stat/guest", { body: body.to_json }))
       end
 
       def set_guestlogin_settings(portal_enabled,
@@ -26,20 +26,17 @@ module Unifi
                  expire_number: expire_number,
                  expire_unit: expire_unit,
                  site_id: site_id}
-        response = self.class.get("/s/#{site}/set/setting/guest_access", { body: body.to_json })
-        response.parsed_response
+        normalize_unifi_response(self.class.get("/s/#{site}/set/setting/guest_access", { body: body.to_json }))
       end
 
       def extend_guest_validity(guest_id, site = @site)
         body = { id: guest_id, cmd: 'extend' }
-        response = self.class.get("/s/#{site}/cmd/hotspot", { body: body.to_json })
-        response.parsed_response
+        normalize_unifi_response(self.class.get("/s/#{site}/cmd/hotspot", { body: body.to_json }))
       end
 
       def unauthorize_guest(mac, site = @site)
         body = { cmd: 'unauthorize-guest', mac: mac.downcase }
-        response = self.class.post("/s/#{site}/cmd/stamgr", { body: body.to_json })
-        response.parsed_response
+        normalize_unifi_response(self.class.post("/s/#{site}/cmd/stamgr", { body: body.to_json }))
       end
 
       def authorize_guest(mac, minutes, up = nil, down = nil, mbytes = nil, ap_mac = nil, site = @site)
@@ -48,8 +45,7 @@ module Unifi
         body[:down] = down if down
         body[:bytes] = mbytes if mbytes
         body[:ap_mac] = ap_mac if ap_mac
-        response = self.class.post("/s/#{site}/cmd/stamgr", { body: body.to_json })
-        response.parsed_response
+        normalize_unifi_response(self.class.post("/s/#{site}/cmd/stamgr", { body: body.to_json }))
       end
 
     end
